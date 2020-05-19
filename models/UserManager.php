3791a86aafbd8c08aab41e06a75e0dbcefac8390
Madina <benaferi-madina@hotmail.fr>
2020-05-19 15:24:24 +0200

    feat: Add login and logout

--- models/UserManager.php
+++ models/UserManager.php
@@ -14,3 +14,29 @@ function GetAllUsers()
   $response = $PDO->query("SELECT * FROM user ORDER BY nickname ASC");
   return $response->fetchAll();
 }
+
+function GetUserIdFromUserAndPassword($username, $password)
+{
+  global $PDO;
+
+  //On verifie que les champs sont remplis
+  if (!empty($username) && !empty($password)) {
+
+    //Request to get the user ID and the PWD
+    $preReq = $PDO->prepare("SELECT * FROM user WHERE nickname = :username and password = :password");
+    $preReq->execute(
+      array(
+        "username" => $username,
+        "password" => $password
+      )
+    );
+  }
+
+  $users = $preReq->fetchAll();
+  //If user exists
+  if (count($users) == 1) {
+    return $users[0]['id'];
+  } else {
+    return -1;
+  }
+}
